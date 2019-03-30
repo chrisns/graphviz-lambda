0cd9c47eb89f989c51153df43cb299a653e97865
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-03-30 16:37:26 +0000

    shorter urls

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var ApiBuilder = require('claudia-api-builder'),
-  api = new ApiBuilder();
-module.exports = api;
+  api = new ApiBuilder()
+module.exports = api
 
-const rp = require('request-promise-native');
+const rp = require('request-promise-native')
 
-const request = require("request");
-const child = require('child_process');
-const exec = require('child_process').exec;
+const child = require('child_process')
 
-api.get('', (request) =>
-    rp(`https://raw.githubusercontent.com/${request.queryString.dot}`)
-      .then((graph) => child.spawnSync("./dot_static", ["-Tsvg"], {input: graph}).stdout.toString())
-  , {success: {contentType: 'image/svg+xml'}});
+const handler = url => rp(`https://raw.githubusercontent.com/${url}`)
+  .then(graph => child.spawnSync("./dot_static", ["-Tsvg"], { input: graph }).stdout.toString())
+
+api.get('', request => handler(request.queryString.dot), { success: { contentType: 'image/svg+xml' } })
+
+api.get('/{dot+}', request => handler(request.pathParams.dot), { success: { contentType: 'image/svg+xml' } })
